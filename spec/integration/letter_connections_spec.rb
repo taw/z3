63ffda346bf600b34c50b4bc74867001e230e597
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-02-21 15:36:00 +0000

    Letter Connections Solver

---
+++ spec/integration/letter_connections_spec.rb
@@ -0,0 +1,17 @@
+describe "Letter Connections" do
+  let(:executable) { "#{__dir__}/../../examples/letter_connections_solver" }
+  it "can solve letter connections puzzle" do
+    expect(IO.popen(executable).read.gsub(/ *$/, "")).to eq <<EOF
+→k →k *K *J →f →f →f →f *F *E
+↑k ↓F →J ↑j ↑f →e →e →e →e ↑e
+↑k →f →f →f ↑f ↑e *G ←g ←g ←g
+↑k ←K →i *I →e ↑e →b →b ↓b ↑g
+→i →i ↑i →e ↑e →b ↑b ↓D ↓b ↑g
+↑I →E →e ↑e →b ↑b ↓d ←d *B ↑g
+↓h ←h ←h ←H ↑b ↓d ←d *C *D ↑g
+↓h →a →a *A ↑b ↓d →C ↑c ↑d ↑g
+↓h ↑A →h *H ↑b →d →d →d ↑d ↑g
+→h →h ↑h →B ↑b →G →g →g →g ↑g
+EOF
+  end
+end
