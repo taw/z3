26f97e9a0beababfb8a60f1d1585fef6280ae6c1
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-04-27 02:28:47 +0200

    Fixing some integration specs
    Updated printer still can't deal with applications,
    and now has new problem with double-escaped special characters,
    but it's still huge progress compared with previous printed (#to_s == #sexpr)

--- spec/integration/algebra_problems_spec.rb
+++ spec/integration/algebra_problems_spec.rb
@@ -3,27 +3,27 @@ describe "Algebra Problems" do
     expect("algebra_problems").to have_output <<EOF
 Solution to problem 01:
 Solution to problem 03:
-* x = (- 22.0)
-* |-6| = 6.0
-* |x-2| = 24.0
+* x = -22
+* |\\|-6\\|| = 6
+* |\\|x-2\\|| = 24
 Solution to problem 04:
-* ax = (- 4.0)
-* ay = (- 5.0)
-* bx = (- 1.0)
-* by = (- 1.0)
-* |a-b| = 5.0
+* ax = -4
+* ay = -5
+* bx = -1
+* by = -1
+* |\\|a-b\\|| = 5
 Solution to problem 05:
-* x = (/ 9.0 2.0)
-* y = 0.0
+* x = 9/2
+* y = 0
 Solution to problem 06:
-* answer = 6.0
-* x1 = 1.0
-* x2 = 2.0
-* y1 = 7.0
-* y2 = 13.0
+* answer = 6
+* x1 = 1
+* x2 = 2
+* y1 = 7
+* y2 = 13
 Solution to problem 10:
-* x = 1.0
-* |-2x + 2| = 0.0
+* x = 1
+* |\\|-2x + 2\\|| = 0
 EOF
   end
 end
--- spec/integration/geometry_problem_spec.rb
+++ spec/integration/geometry_problem_spec.rb
@@ -1,16 +1,16 @@
 describe "Geometry Problem" do
   it do
     expect("geometry_problem").to have_output <<EOF
-* a.x = 0.0
+* a.x = 0
 * a.y = (root-obj (+ (^ x 2) (- 300)) 1)
-* b.x = 0.0
-* b.y = 0.0
-* c.x = 10.0
-* c.y = 0.0
-* d.x = 10.0
+* b.x = 0
+* b.y = 0
+* c.x = 10
+* c.y = 0
+* d.x = 10
 * d.y = (root-obj (+ (^ x 2) (- 300)) 1)
-* |a-c| = (- 20.0)
-* |b-d| = 20.0
+* |\\|a-c\\|| = -20
+* |\\|b-d\\|| = 20
 EOF
   end
 end
