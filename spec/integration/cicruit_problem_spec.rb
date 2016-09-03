b758479b9de9f01222370fd5d407d47c65e9de7f
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-04 00:46:17 +0100

    Simply rectifier circuit solved

--- spec/integration/cicruit_problem_spec.rb
+++ spec/integration/cicruit_problem_spec.rb
@@ -6,6 +6,18 @@ Problem 1
 
 Problem 2
 * I V = -10
+
+Problem 3
+* I L = -1
+* I V = -1
+* I L = -1/2
+* I V = -1/2
+* I L = 0
+* I V = 0
+* I L = -1/2
+* I V = 1/2
+* I L = -1
+* I V = 1
 EOF
   end
 end
