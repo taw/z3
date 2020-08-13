1bde4df784e29069f82aaa653090a1508bb2eeb9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-13 04:53:36 +0100

    ABC Path solver

---
+++ spec/integration/abc_path_spec.rb
@@ -0,0 +1,19 @@
+describe "ABC Path" do
+  it do
+    expect("abc_path").to have_output <<'EOF'
+C O R D F B U
+
+N o-n l-k-j L
+  | |/    |
+G p m g-f i I
+  |    x /
+Q q-r e h a H
+   /  |   |
+V s v d-c-b S
+   x \
+T u-t w-x-y X
+
+J P M W K Y E
+EOF
+  end
+end
