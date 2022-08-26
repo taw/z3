dc60e4dea222bfd37fb69aa2e92a93cb03fcc142
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-08-27 00:32:05 +0100

    More versions

--- spec/integration/abc_path_spec.rb
+++ spec/integration/abc_path_spec.rb
@@ -1,21 +1,5 @@
-# There are multiple solutions, so this test is nondeterministic
-# This is what Z3 4.8.13 returns
 describe "ABC Path" do
   it do
-    expect("abc_path").to have_output <<'EOF'
-C O R D F B U
-
-N o-n l-k-j L
-  | |/    |
-G p m g-f i I
-  |    x /
-Q q-r e h a H
-   /  |   |
-V s v d-c-b S
-   x \
-T u-t w-x-y X
-
-J P M W K Y E
-EOF
+    expect("abc_path").to have_output_matching_saved_example
   end
 end
--- spec/integration/knights_puzzle_spec.rb
+++ spec/integration/knights_puzzle_spec.rb
@@ -1,5 +1,3 @@
-# There are multiple solutions, so this test is nondeterministic
-# This is what Z3 4.8.13 returns
 describe "Knights Swap Puzzle" do
   it do
     expect("knights_puzzle").to have_output_matching_saved_example
