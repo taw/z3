3c6e17612ea60677faa575ac996616121dd740de
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-08-27 00:45:38 +0100

    More examples to the list

--- spec/integration/futoshiki_spec.rb
+++ spec/integration/futoshiki_spec.rb
@@ -1,23 +1,5 @@
 describe "Futoshiki" do
   it do
-    expect("futoshiki").to have_output <<EOF
-2 5 3 4 1 7 6<9 8
-
-1 9 7 2 8 5 3<4<6
-  _
-7<8 2 5>3>1 4<6 9
-^
-8 1<5<9 7 6 2 3 4
-            ^
-4 7>6>1 9 2 5 8 3
-^         ^     ^
-6 4 1 8 2 3 9 5 7
-  _   _ ^   _ _
-9 3 4 6>5 8 7 1 2
-_
-5 2 9>3 6>4 8>7 1
-              _
-3 6 8>7>4 9 1<2 5
-EOF
+    expect("futoshiki").to have_output_matching_saved_example
   end
 end
