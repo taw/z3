01f61d6eeb785958f43d2298a301c11232994315
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2022-10-20 15:04:15 +0100

    Z3::Array store and select

--- lib/z3/expr/array_expr.rb
+++ lib/z3/expr/array_expr.rb
@@ -1,5 +1,25 @@
 module Z3
   class ArrayExpr < Expr
     public_class_method :new
+
+    def key_sort
+      sort.key_sort
+    end
+
+    def value_sort
+      sort.value_sort
+    end
+
+    def store(key, value)
+      sort.new LowLevel.mk_store(self, key_sort.cast(key), value_sort.cast(value))
+    end
+
+    def select(key)
+      sort.value_sort.new LowLevel.mk_select(self, key_sort.cast(key))
+    end
+
+    def [](key)
+      select(key)
+    end
   end
 end
--- spec/array_expr_spec.rb
+++ spec/array_expr_spec.rb
@@ -4,7 +4,9 @@ module Z3
     let(:a) { sort.var("a") }
     let(:b) { sort.var("b") }
     let(:c) { sort.var("c") }
-    let(:x) { Z3::Bool("x") }
+    let(:x) { Z3::Int("x") }
+    let(:y) { Z3::Int("y") }
+    let(:z) { Z3::Int("z") }
 
     # TODO: Formatting is dreadful
     it "== and !=" do
@@ -12,6 +14,36 @@ module Z3
         a => "store(const(0), 0, 1)",
         b => "store(const(0), 0, 1)",
         c => "const(0)",
+      )
+    end
+
+    it "select" do
+      expect([a.select(10) == 20]).to have_solution(
+        a => "const(20)",
+      )
+      expect([a[10] == 20]).to have_solution(
+        a => "const(20)",
+      )
+      expect([a[x] == 10, a[y] == 20]).to have_solution(
+        a => "store(const(10), 3, 20)",
+        x => "2",
+        y => "3",
+      )
+    end
+
+    it "store" do
+      expect([a == b.store(10, 20), x == a.select(10)]).to have_solution(
+        x => 20,
+      )
+      expect([a == b.store(10, 20), x == a[10]]).to have_solution(
+        x => 20,
+      )
+      expect([a == b.store(10, 20), x == a[y], y == 10]).to have_solution(
+        x => 20,
+      )
+      expect([a == b.store(10, 20), x == a[y], x == 20]).to have_solution(
+        x => 20,
+        y => 10,
       )
     end
   end
