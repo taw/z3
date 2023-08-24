09f001f9092f577982e62bf472b1956f4016f71a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2023-08-24 09:25:02 +0100

    A few more examples of what solutions can be

--- spec/integration/cats_organized_neatly_spec.rb
+++ spec/integration/cats_organized_neatly_spec.rb
@@ -1,14 +1,5 @@
 describe "Cats Organized Neatly" do
   it do
-    expect("cats_organized_neatly").to have_output <<EOF
-.ffffbbb
-.fdddbbb
-aadddbbb
-aadddbbe
-aagggcee
-aagggcce
-aagggcc.
-..gggcc.
-EOF
+    expect("cats_organized_neatly").to have_output_matching_saved_example
   end
 end
