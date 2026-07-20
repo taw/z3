cf88f1c88a152f43bf47f145ee5ec6554752eaec
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-07-20 01:00:33 +0100

    Fixed Light Up puzzle specification

--- spec/integration/light_up_spec.rb
+++ spec/integration/light_up_spec.rb
@@ -1,4 +1,12 @@
 describe "Light Up" do
+  let(:binary) { Pathname(__dir__) + "../../examples/light_up" }
+
+  # Walls don't need to be lit, only white cells do
+  it "solves boards where a wall has no line of sight to any lamp" do
+    board = Pathname(__dir__) + "../../examples/light_up-2.txt"
+    expect(`#{binary} #{board}`).to eq("0 \n *\n")
+  end
+
   it do
     expect("light_up").to have_output <<EOF
   * 0
