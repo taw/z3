474dce1e10e460852001beb8518e33c5dcc690f3
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2026-08-02 12:39:09 +0100

    CharSort.from_const

--- lib/z3/sort/char_sort.rb
+++ lib/z3/sort/char_sort.rb
@@ -4,8 +4,27 @@ module Z3
       super LowLevel.mk_char_sort
     end
 
+    # Z3's alphabet is Unicode code points 0 to 0x2FFFF, so it stops short of
+    # Ruby's 0x10FFFF. Z3 itself doesn't check, it just misbehaves.
+    MAX_CODE_POINT = 0x2FFFF
+
     def expr_class
       Expr
+    end
+
+    # Ruby has no character type, so a Char can be built either from a code point
+    # or from a one character String
+    def from_const(val)
+      case val
+      when Integer
+        raise Z3::Exception, "Char code point must be between 0 and 0x#{MAX_CODE_POINT.to_s(16).upcase}" unless (0..MAX_CODE_POINT).cover?(val)
+        new(LowLevel.mk_char(val))
+      when String
+        raise Z3::Exception, "Only single character strings can be converted to Char" unless val.size == 1
+        from_const(val.ord)
+      else
+        raise Z3::Exception, "Cannot convert #{val.class} to #{self.class}"
+      end
     end
 
     def to_s
--- spec/char_sort_spec.rb
+++ spec/char_sort_spec.rb
@@ -18,5 +18,37 @@ module Z3
     it "can instantiate variables" do
       expect(sort.var("c").inspect).to eq("Char<c>")
     end
+
+    it "can instantiate constants from code points" do
+      expect(sort.from_const(97).inspect).to eq(%q{Char<Char("a")>})
+      expect(sort.from_const(0).inspect).to eq(%q{Char<Char("\u0000")>})
+      expect(sort.from_const(0x4e2d).inspect).to eq(%q{Char<Char("中")>})
+      expect(sort.from_const(0x2FFFF).sexpr).to eq("(_ Char 196607)")
+    end
+
+    it "can instantiate constants from single character strings" do
+      expect(sort.from_const("a").inspect).to eq(sort.from_const(97).inspect)
+      expect(sort.from_const("\n").inspect).to eq(sort.from_const(10).inspect)
+      expect(sort.from_const("中").inspect).to eq(sort.from_const(0x4e2d).inspect)
+    end
+
+    # Z3 doesn't check this itself, it just silently makes a character
+    # that no solution can ever contain
+    it "raises exception when code point is out of range" do
+      expect{ sort.from_const(-1) }.to raise_error(Z3::Exception)
+      expect{ sort.from_const(0x30000) }.to raise_error(Z3::Exception)
+      expect{ sort.from_const("\u{10FFFF}") }.to raise_error(Z3::Exception)
+    end
+
+    it "raises exception when string is not a single character" do
+      expect{ sort.from_const("") }.to raise_error(Z3::Exception)
+      expect{ sort.from_const("ab") }.to raise_error(Z3::Exception)
+    end
+
+    it "raises exception when trying to convert constants of wrong type" do
+      expect{ sort.from_const(true) }.to raise_error(Z3::Exception)
+      expect{ sort.from_const(97.0) }.to raise_error(Z3::Exception)
+      expect{ sort.from_const(nil) }.to raise_error(Z3::Exception)
+    end
   end
 end
